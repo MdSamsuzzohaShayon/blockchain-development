7bc4beba4f832991a6b132e8b0d1da51b89c7f6b
Md Shayon <mdshayon0@gmail.com>
2024-09-05 18:25:19 +0600

    Testing with ethereum mainnet

--- 06-foundry-fund-me/commands.sh
+++ 06-foundry-fund-me/commands.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# forge init
-# forge test
-# forge compile
-# forge build
-# forge test -vv
-# forge test --match-test testPriceFeedVersion -vvv
-# forge script script/DeployFundMe.s.sol
-# forge test --match-test testPriceFeedVersion -vvv --fork-url $SEPOLIA_RPC_URL
-# forge test --match-test testPriceFeedVersion -vvv --rpc-url $SEPOLIA_RPC_URL
-# forge coverage --rpc-url $SEPOLIA_RPC_URL
-# forge test --rpc-url $SEPOLIA_RPC_URL
+forge init
+forge test
+forge compile
+forge build
+forge test -vv
+forge test --match-test testPriceFeedVersion -vvv
+forge script script/DeployFundMe.s.sol
+forge test --match-test testPriceFeedVersion -vvv --fork-url $SEPOLIA_RPC_URL
+forge test --match-test testPriceFeedVersion -vvv --rpc-url $SEPOLIA_RPC_URL
+forge coverage --rpc-url $SEPOLIA_RPC_URL
+forge test --rpc-url $SEPOLIA_RPC_URL
+forge test --fork-url $MAINNET_RPC_URL
--- 06-foundry-fund-me/script/HelperConfig.sol
+++ 06-foundry-fund-me/script/HelperConfig.sol
@@ -11,7 +11,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 
 
-contract HelperConfig{
+contract HelperConfig is Script{
     // If we are on a local anvil, we deploy mocks
     // Otherwise, gtrab the existing address from the live network
 
@@ -27,20 +27,43 @@ contract HelperConfig{
          * https://docs.soliditylang.org/en/latest/units-and-global-variables.html#block-and-transaction-properties
          * See all chain IDs https://chainlist.org/
          * Sepolia chainid = 11155111
+         * Ethereum chainid = 1
          */
         if(block.chainid == 11155111){
             activeNetworkConfig = getSepoliaEthConfig();
-        }else{
+        }else if (block.chainid == 1){
+            activeNetworkConfig = getMainnetEthConfig();
+        } else{
             activeNetworkConfig = getAnvilEthConfig();
         }
     }
 
     function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
-        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        /**
+         * Price Feed Contract Addresses: https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1
+         */
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306}); // Sepolia Mainnet -> ETH / USD
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns(NetworkConfig memory){
+    function getMainnetEthConfig() public pure returns(NetworkConfig memory){
+        /**
+         * Price Feed Contract Addresses: https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1
+         */
+        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419}); // Ethereum Mainnet -> ETH / USD
+        return ethConfig;
+    }
+
+    function getAnvilEthConfig() public returns(NetworkConfig memory){
+        // Deploy the mock
+        // Return the mock address 
+
+        // NetworkConfig memory ethConfig = NetworkConfig({
+        //     priceFeed: 
+        // });
+
+        vm.startBroadcast();
+        vm.stopBroadcast();
 
     }
 }
