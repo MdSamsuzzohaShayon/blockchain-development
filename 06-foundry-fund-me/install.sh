eb764846e21710505183721470eb1d59c8cb6731
Md Shayon <mdshayon0@gmail.com>
2024-09-13 15:43:58 +0600

    Clean up

--- 06-foundry-fund-me/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# https://github.com/smartcontractkit/chainlink-brownie-contracts
-forge install smartcontractkit/chainlink-brownie-contracts --no-commit
-# Recommended DevOps: foundry-devops - https://github.com/Cyfrin/foundry-devops
-forge install ChainAccelOrg/foundry-devops --no-commit
