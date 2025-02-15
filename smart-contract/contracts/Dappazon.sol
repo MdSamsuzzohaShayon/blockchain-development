852403a5578e0ebce6fc29e25d18b80efd5a075e
Md Shayon <mdshayon0@gmail.com>
2025-02-16 03:11:54 +0600

    List or adding product successfully

--- smart-contract/contracts/Dappazon.sol
+++ smart-contract/contracts/Dappazon.sol
@@ -8,10 +8,53 @@ contract Dappazon {
     string public name;
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
 
+    mapping(uint256 => Item) public items;
+    event List(string name, uint256 cost, uint256 quantity);
 
-    constructor(){
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is able to do this!");
+        _;
+    }
+    constructor() {
         name = "Dappazon";
         owner = msg.sender; // Who deploy the smart contract
     }
+
+    // List/add products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner{
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        items[_id] = item;
+        emit List(_name, _cost, _stock);
+    }
+
+    // Buy products
+
+    // Withdraw funds
 }
--- smart-contract/test/Dappazon.ts
+++ smart-contract/test/Dappazon.ts
@@ -1,7 +1,20 @@
 import { Dappazon } from "../typechain-types"; // Import the generated contract type
 import { expect } from "chai";
-import { Signer } from "ethers";
+import { ContractTransactionResponse, Signer } from "ethers";
 import hre from "hardhat";
+
+// convert eth to wei 
+const tokens = (r: number)=>{
+  return hre.ethers.parseUnits(r.toString(), 'ether');
+}
+
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE = "https://bafybeianqgchjc7ztelteo5mvlofkj734zd6bo5ztsraeygxqtas7zd57m.ipfs.dweb.link?filename=shoes.jpg";
+const COST = tokens(1); // 1 ether
+const RATING = 4;
+const STOCK = 5;
 
 describe("Dappazon", function () {
   let dappazon: Dappazon; // Use the generated contract type
@@ -18,14 +31,45 @@ describe("Dappazon", function () {
     await dappazon.waitForDeployment(); // Ensures the contract is fully deployed
   });
 
-  describe("Deployment", ()=>{
-    it("Should sets the owner", async ()=>{
+  describe("Deployment", () => {
+    it("Should sets the owner", async () => {
       expect(await dappazon.owner()).to.equal(deployer);
     });
     it("Should have a name", async () => {
       const name: string = await dappazon.name(); // Type is automatically inferred
       expect(name).to.equal("Dappazon");
     });
-  })
+  });
+
+  describe("Listing", () => {
+    let transaction: ContractTransactionResponse;
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      );
+    });
+
+    it("Should return item attributes", async () => {
+      const item = await dappazon.items(ID);
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+
+    it("Should emit list event", async () => {
+      expect(transaction).to.emit(dappazon, "List");
+    });
+  });
 
 });
